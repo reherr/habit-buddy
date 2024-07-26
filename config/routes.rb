16b9eeda05cf67d75a42e70bc77f91fbf1d3f554
Rebecca Herrera <rmherrera.16@gmail.com>
2024-07-26 10:52:20 -0500

    added a current_streak feature to habits#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  # get 'home/index'
+
   resources :habit_entries
   resources :habits
   devise_for :users
 
   root to: "home#index"
 
-  # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
-
-  # get "/your_first_screen" => "pages#first"
   
 end
