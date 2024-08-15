3cc7dd729fb4f33f8ed65345938f5b9961fc5ce0
Rebecca Herrera <rmherrera.16@gmail.com>
2024-08-15 11:26:32 -0500

    created partials for home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,11 @@
 class HomeController < ApplicationController
-  # before_action :authenticate_user!, except: [:index]
   before_action :redirect_if_signed_in
 
-  def index
-
-  end
+  def index; end
 
   private
 
   def redirect_if_signed_in
-   redirect_to habits_path if user_signed_in?
+    redirect_to habits_path if user_signed_in?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,8 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
+  draw :admin
+  draw :service_worker
   root to: 'home#index'
-  
   resources :habit_entries
   resources :habits
   devise_for :users
-  
-  authenticate :user, ->(user) { user.admin? } do
-    mount Blazer::Engine, at: 'blazer'
-    mount RailsAdmin::Engine, at: 'admin', as: 'rails_admin'
-  end
-
-  get "/manifest.json", to: "service_worker#manifest"
-
 end
